9e9da8acf1f8c97ec1907c5547349b18e89eb864
tanvibhakta <tanvibhakta@gmail.com>
2018-01-22 12:38:11 +0530

    add parameter checking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,15 @@ class ArticlesController < ApplicationController
 
   def create
       # render plain: params[:articles].inspect
-      @article = Article.new(params[:article])
+      @article = Article.new(article_params)
 
       @article.save
       redirect_to @article
   end
-  
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
 end
